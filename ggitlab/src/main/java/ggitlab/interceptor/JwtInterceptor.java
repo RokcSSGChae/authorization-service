b3bc5bf027cbe165f6416dd8f56b21d6176a885a
Seokchae Park <seokcpark@smilegate.com>
2020-12-17 15:02:07 +0900

    feat(Interceptor): JWT 확인하는 JwtInterceptor 추가 및 WebConfig 클래스 추가

---
+++ ggitlab/src/main/java/ggitlab/interceptor/JwtInterceptor.java
@@ -0,0 +1,31 @@
+package ggitlab.interceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+import ggitlab.error.UnauthorizedException;
+import ggitlab.service.JwtService;
+
+@Component
+public class JwtInterceptor implements HandlerInterceptor {
+	
+	private static final String HEADER_AUTH = "Authorization";
+
+	@Autowired
+	private JwtService jwtService;
+
+	@Override
+	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
+			throws Exception {
+		final String token = request.getHeader(HEADER_AUTH);
+		if (token != null && jwtService.isUsable(token)) {
+			return true;
+		} else {
+			throw new UnauthorizedException();
+		}
+	}
+}
